e70e24e01f1c2309ba13ae62c1ff4aafb26880b1
Mohamad Mrad <m7amadmr4d@gmail.com>
2025-09-18 20:08:23 +0300

    Update Vidration Table.ino

--- Vibration table/Vibration Table Code/Vidration Table.ino
+++ Vibration table/Vibration Table Code/Vidration Table.ino
@@ -7,19 +7,13 @@ void setup() {
   pinMode(enaPin, OUTPUT);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
-  
   digitalWrite(in1Pin, HIGH);
   digitalWrite(in2Pin, LOW);
 }
 
 void loop() {
-  
   int potValue = analogRead(potPin);
-
-
   int pwmValue = map(potValue, 0, 1023, 0, 255);
- 
   analogWrite(enaPin, pwmValue);
-
   delay(10); 
 }
